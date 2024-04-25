00c46a1d045cfecee1e28a35319cfe593ed68eaf
Dodi <dodipras27@gmail.com>
2024-04-25 23:57:05 +0700

    [UPDATE] fix script

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   description = "The project_id to deploy the example instance into.  (e.g. \"simple-sample-project-1234\")"
+  default = "interview-417918"
 }
 
 variable "region" {
   description = "The region to deploy to"
+  default = "asia-southeast1"
 }
 
 variable "network" {
